3738de08aeb7b957eefdabc6e9fcbfbcfb95df57
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-13 08:50:24 +0100

    Create Druga vjezba.php

---
+++ Druga vjezba.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+2. Write a PHP program to get the absolute difference between n and 51. 
+If n is greater than 51 return triple the absolute difference.
+Sample Input:
+55
+30
+51
+Sample Output:
+6
+21
+0
+*/
+
+function test($n) 
+
+{
+    $x = 51;
+
+    if ($n > $x){
+        return ($n - $x)*3;
+    }
+        return $x - $n;
+}
+
+echo test (55) . "\n";
+echo test (30) . "\n";
+echo test (51) . "\n";  
