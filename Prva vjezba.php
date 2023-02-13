89787fbcb70ce53640fe2ad3693f098e59e2359f
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-13 08:50:28 +0100

    Update Prva vjezba.php

--- Prva vjezba.php
+++ Prva vjezba.php
@@ -18,5 +18,5 @@ function test($x, $y)
     return $x == $y ? ($x + $y)*3 : $x + $y;
 }
 echo test (1, 2) . "\n";
-echo test (3, 3) . "\n";
-echo test (2, 12) . "\n";   
+echo test (3, 2) . "\n";
+echo test (2, 2) . "\n";   
