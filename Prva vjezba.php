13eadbfffd2171a3ee5b1a4e6c5e2186d3071936
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-13 08:45:21 +0100

    Create Prva vjezba.php

---
+++ Prva vjezba.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+1. Write a PHP program to compute the sum of the two given integer values.
+If the two values are the same, then returns triple their sum.
+Sample Input:
+1, 2
+3, 2
+2, 2
+Sample Output:
+3
+5
+12
+*/
+
+function test($x, $y) 
+{
+    return $x == $y ? ($x + $y)*3 : $x + $y;
+}
+echo test (1, 2) . "\n";
+echo test (3, 3) . "\n";
+echo test (2, 12) . "\n";   
